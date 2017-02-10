38cc087fd787672a929792042258415a81fa29fb
Nan YE <dailystudio2010@gmail.com>
2017-02-10 16:02:10 +0800

    [*][DEV]: fix potential crash issue in AbsLoaderFragment;

--- src/main/java/com/dailystudio/app/fragment/AbsLoaderFragment.java
+++ src/main/java/com/dailystudio/app/fragment/AbsLoaderFragment.java
@@ -3,16 +3,22 @@ package com.dailystudio.app.fragment;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.ContextCompat;
+
+import com.dailystudio.development.Logger;
 
 public abstract class AbsLoaderFragment<T> extends BaseIntentFragment implements LoaderCallbacks<T> {
     
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		
-		getLoaderManager().initLoader(getLoaderId(), createLoaderArguments(), this);
+
+		LoaderManager ldMgr = getLoaderManagerSafe();
+		if (ldMgr != null) {
+			ldMgr.initLoader(getLoaderId(), createLoaderArguments(), this);
+		}
 	}
 	
 	public void onNewIntent(Intent intent) {
@@ -22,7 +28,26 @@ public abstract class AbsLoaderFragment<T> extends BaseIntentFragment implements
 	}
 	
 	public void restartLoader() {
-		getLoaderManager().restartLoader(getLoaderId(), createLoaderArguments(), this);
+		LoaderManager ldMgr = getLoaderManagerSafe();
+		if (ldMgr != null) {
+			ldMgr.restartLoader(getLoaderId(), createLoaderArguments(), this);
+		}
+	}
+
+	private LoaderManager getLoaderManagerSafe() {
+		LoaderManager loaderManager;
+
+		try {
+			loaderManager = getLoaderManager();
+
+		} catch (IllegalStateException e) {
+			Logger.warn("could not get loader manager: %s",
+					e.toString());
+
+			loaderManager = null;
+		}
+
+		return loaderManager;
 	}
 
 	protected int getEmptyViewId() {
